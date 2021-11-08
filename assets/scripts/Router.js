cebe10032955c178bfd609428db1e7eb5fbcb814
ctyuucsdedu <ctyu@ucsd.edu>
2021-11-07 17:04:27 -0800

    updated router

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -81,7 +81,6 @@ export class Router {
 
     if (!statePopped && window.location.hash != hash) {
       history.pushState(page, page, (window.location.origin + hash));
-      console.log(window.location);
     }
 
     this[page]();
